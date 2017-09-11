29b53dc99e9a6b214a22b66d365253e00c335157
Bruno Ferreira <chalkos@chalkos.net>
2017-09-11 14:22:36 +0100

    removed old ROOT folder, replaced it with ROOT.war based on version 1.0.2

--- 03-db-visualization-toolkit/apache-tomcat/webapps/ROOT/WEB-INF/classes/config/theme/theme.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    if(/branding=false/.test(window.location.search)) {
-        $("head").append('<link rel="stylesheet" type="text/css" href="api/v1/theme?resource_id=nobranding.css">');
-    }
-});
